dbcaa2447e14c425f4810e44db5e07f60154d9aa
Repnikov.A <repnikov.a@florcom.ru>
2023-09-28 11:27:22 +0300

    15.2.8

--- Modul15/Program.cs
+++ Modul15/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        var numList = new List<double>();
+
+        while (true)
+        {
+            Console.WriteLine("Введите число");
+            bool rightNum = double.TryParse(Console.ReadLine(), out double num);
+
+            if (!rightNum)
+                Console.WriteLine("Ошибка ввода, введите число");
+            else
+            {
+                numList.Add(num);
+
+                Console.WriteLine($"Количество чисел в списке: {numList.Count}");
+                Console.WriteLine($"Сумма чисел в списке: {numList.Sum()}");
+                Console.WriteLine($"Наибольшее число в списке: {numList.Max()}");
+                Console.WriteLine($"Наименьшее число в списке: {numList.Min()}");
+                Console.WriteLine($"Среднее значение чисел в списке: {numList.Average()}");
+
+            }
+        }
+    }
+
+}
+
