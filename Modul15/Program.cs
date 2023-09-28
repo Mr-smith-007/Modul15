c2eacb1aa206e5db83de60f172962d276294eff5
Repnikov.A <repnikov.a@florcom.ru>
2023-09-28 11:55:14 +0300

    15.3.3

--- Modul15/Program.cs
+++ Modul15/Program.cs
@@ -9,28 +9,56 @@ class Program
 
     static void Main(string[] args)
     {
-        var numList = new List<double>();
+        var phoneBook = new List<Contact>();
 
-        while (true)
+        phoneBook.Add(new Contact("Игорь", 79990000001, "igor@example.com"));
+        phoneBook.Add(new Contact("Сергей", 79990000010, "serge@example.com"));
+        phoneBook.Add(new Contact("Анатолий", 79990000011, "anatoly@example.com"));
+        phoneBook.Add(new Contact("Валерий", 79990000012, "valera@example.com"));
+        phoneBook.Add(new Contact("Сергей", 799900000013, "serg@gmail.com"));
+        phoneBook.Add(new Contact("Иннокентий", 799900000013, "innokentii@example.com"));
+
+        var grouped = phoneBook.GroupBy(c => c.Email.Split("@").Last());
+
+        
+        foreach (var group in grouped)
         {
-            Console.WriteLine("Введите число");
-            bool rightNum = double.TryParse(Console.ReadLine(), out double num);
-
-            if (!rightNum)
-                Console.WriteLine("Ошибка ввода, введите число");
-            else
+            
+            if (group.Key.Contains("example"))
             {
-                numList.Add(num);
+                Console.WriteLine("Фейковые адреса: ");
 
-                Console.WriteLine($"Количество чисел в списке: {numList.Count}");
-                Console.WriteLine($"Сумма чисел в списке: {numList.Sum()}");
-                Console.WriteLine($"Наибольшее число в списке: {numList.Max()}");
-                Console.WriteLine($"Наименьшее число в списке: {numList.Min()}");
-                Console.WriteLine($"Среднее значение чисел в списке: {numList.Average()}");
+                foreach (var contact in group)
+                    Console.WriteLine(contact.Name + " " + contact.Email);
 
             }
+            else
+            {
+                Console.WriteLine("Реальные адреса: ");
+                foreach (var contact in group)
+                    Console.WriteLine(contact.Name + " " + contact.Email);
+            }
+
+            Console.WriteLine();
         }
+
     }
 
 }
+public class Contact
+{
+    public Contact(string name, long phoneNumber, String email)
+    {
+        Name = name;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+
+    public String Name { get; }
+    public long PhoneNumber { get; }
+    public String Email { get; }
+}
+
+
+
 
